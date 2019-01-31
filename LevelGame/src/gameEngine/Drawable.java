841bcefacc5d6d1ef7960b2bdfbba8b8682832b2
lukeleonard <45070596+lukeleonard@users.noreply.github.com>
2019-01-31 14:07:59 -0700

    Initial game file
    Initial commit

---
+++ LevelGame/src/gameEngine/Drawable.java
@@ -0,0 +1,13 @@
+package gameEngine;
+
+/**
+ * All game pieces may be "drawn" - just printed on console, 
+ * no parameters needed.
+ * 
+ * @author Mark Baldwin
+ * @author Cyndi Rader
+ * 
+ */
+public interface Drawable {
+	public void draw();
+}
